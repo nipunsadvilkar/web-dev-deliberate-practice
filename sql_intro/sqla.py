1c6edb86edc11523350f76f46fe963d9709e6311
Nipun Sadvilkar <nipunsadvilkar@gmail.com>
2018-04-21 20:57:17 +0530

    Insert many rows in DB with parameterized statement

--- sql_intro/sqla.py
+++ sql_intro/sqla.py
@@ -4,15 +4,22 @@ Explore SQLite functionalty
 import sqlite3
 
 # connect with given DB otherwise create new database
-conn = sqlite3.connect('cars.db')
+conn = sqlite3.connect('new.db')
 
 # get a cursor object used to execute SQL commands
 cursor = conn.cursor()
 
 # create a table with given column
-cursor.execute("""CREATE TABLE inventory
-                (make TEXT, model TEXT, quantity INT)
+cursor.execute("""CREATE TABLE population
+                (city TEXT, state TEXT, population INT)
                 """)
+
+cursor.execute("INSERT INTO population VALUES('NEW YORK CITY', \
+        'NY', '8200000')")
+cursor.execute("INSERT INTO population VALUES('SAN FRANCISCO', \
+        'CA', '8000000')")
+
+conn.commit()
 
 # close the database connection
 conn.close()
