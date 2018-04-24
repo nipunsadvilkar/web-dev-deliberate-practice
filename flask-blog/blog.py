964c9e0f5ef46484054e6b78547ffbbd0421e61c
Nipun Sadvilkar <nipunsadvilkar@gmail.com>
2018-04-24 23:58:06 +0530

    Add user login and logout feature with session

--- flask-blog/blog.py
+++ flask-blog/blog.py
@@ -3,6 +3,9 @@ from flask import Flask, render_template, request, session, flash, redirect, \
 import sqlite3
 
 DATABASE = 'blog.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = '\xf7\x94\x0c\xdb\x14F\xe6X\x1e\xc5]7\x12B\xcd\xf1\xe8G\x84\t\xcb'
 
 app = Flask(__name__)
 
@@ -13,11 +16,25 @@ def connect_db():
 
 @app.route('/')
 def login():
+    error = None
+    if request.method = 'POST':
+        if request.form['username'] != app.config['USERNAME'] or \
+                request.form['password'] != app.config['PASSWORD']:
+                    error = 'Invalid Credentials. Please try again.'
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('main'))
     return render_template('login.html')
 
 @app.route('/main')
 def main():
     return render_template('main.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
