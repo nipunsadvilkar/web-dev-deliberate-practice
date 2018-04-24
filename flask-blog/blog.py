d7ec2a259484c717073371a4c1ad70f8aca24b83
Nipun Sadvilkar <nipunsadvilkar@gmail.com>
2018-04-25 00:27:43 +0530

    Add logout link in main.html & flash error if invalid uname/password

--- flask-blog/blog.py
+++ flask-blog/blog.py
@@ -14,17 +14,17 @@ app.config.from_object(__name__)
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def login():
     error = None
-    if request.method = 'POST':
+    if request.method == 'POST':
         if request.form['username'] != app.config['USERNAME'] or \
                 request.form['password'] != app.config['PASSWORD']:
-                    error = 'Invalid Credentials. Please try again.'
+            error = 'Invalid Credentials. Please try again.'
         else:
             session['logged_in'] = True
             return redirect(url_for('main'))
-    return render_template('login.html')
+    return render_template('login.html', error=error)
 
 @app.route('/main')
 def main():
