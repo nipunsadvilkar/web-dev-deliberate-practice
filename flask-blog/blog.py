1795882f5e80b2707fb75667d9021373ba3ac5c3
Nipun Sadvilkar <nipunsadvilkar@gmail.com>
2018-04-26 00:45:38 +0530

    Add new post feature

--- flask-blog/blog.py
+++ flask-blog/blog.py
@@ -52,5 +52,22 @@ def logout():
     flash('You were logged out')
     return redirect(url_for('login'))
 
+@app.route('/add', methods=['POST'])
+@login_required
+def add():
+    title = request.form['title']
+    post = request.form['post']
+    if not title or not post:
+        flash('All fields are required. Please try again.')
+        return redirect(url_for('main'))
+    else:
+        g.db = connect_db()
+        g.db.execute('INSERT INTO posts (title, post) VALUES (?, ?)',
+                [request.form['title'], request.form['post']])
+        g.db.commit()
+        g.db.close()
+        flash('New entry was successfully posted!')
+        return redirect(url_for('main'))
+
 if __name__ == '__main__':
     app.run(debug=True)
