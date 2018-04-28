bdf81b4f00584e917fe29bd8a5018f2021a15589
Nipun Sadvilkar <nipunsadvilkar@gmail.com>
2018-04-28 22:04:17 +0530

    Flake8 & HTML attributes fixes

--- flask-blog/blog.py
+++ flask-blog/blog.py
@@ -12,8 +12,10 @@ app = Flask(__name__)
 
 app.config.from_object(__name__)
 
+
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
+
 
 def login_required(test):
     @wraps(test)
@@ -24,6 +26,7 @@ def login_required(test):
             flash('You need to login first')
             return redirect(url_for('login'))
     return wrap
+
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
@@ -37,6 +40,7 @@ def login():
             return redirect(url_for('main'))
     return render_template('login.html', error=error)
 
+
 @app.route('/main')
 @login_required
 def main():
@@ -46,11 +50,13 @@ def main():
     g.db.close()
     return render_template('main.html', posts=posts)
 
+
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
     return redirect(url_for('login'))
+
 
 @app.route('/add', methods=['POST'])
 @login_required
@@ -63,11 +69,12 @@ def add():
     else:
         g.db = connect_db()
         g.db.execute('INSERT INTO posts (title, post) VALUES (?, ?)',
-                [request.form['title'], request.form['post']])
+                     [request.form['title'], request.form['post']])
         g.db.commit()
         g.db.close()
         flash('New entry was successfully posted!')
         return redirect(url_for('main'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
