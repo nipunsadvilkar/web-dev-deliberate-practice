361d61f12889588e8bc636baee370977d4a70764
Nipun Sadvilkar <nipunsadvilkar@gmail.com>
2018-05-09 23:36:35 +0530

    Add user_id in sessions & posted by shows name of the user👤

--- FlaskTaskr/views.py
+++ FlaskTaskr/views.py
@@ -45,6 +45,7 @@ def login_required(test):
 @app.route('/logout/')
 def logout():
     session.pop('logged_in', None)
+    session.pop('user_id', None)
     flash('Goodbye!')
     return redirect(url_for('login'))
 
@@ -58,6 +59,7 @@ def login():
             user = User.query.filter_by(name=request.form['name']).first()
             if user is not None and user.password == request.form['password']:
                 session['logged_in'] = True
+                session['user_id'] = user.id
                 flash('Welcome!')
                 return redirect(url_for('tasks'))
             else:
@@ -85,16 +87,21 @@ def tasks():
 def new_task():
     form = AddTaskForm(request.form)
     if request.method == 'POST':
-        new_task = Task(form.name.data,
-                        form.due_date.data,
-                        form.priority.data,
-                        datetime.datetime.utcnow(),
-                        '1',
-                        '1')
-        db.session.add(new_task)
-        db.session.commit()
-        flash('New entry was successfully posted. Thanks!')
-    return redirect(url_for('tasks'))
+        if form.validate():
+            new_task = Task(form.name.data,
+                            form.due_date.data,
+                            form.priority.data,
+                            datetime.datetime.utcnow(),
+                            '1',
+                            session['user_id'])
+            db.session.add(new_task)
+            db.session.commit()
+            flash('New entry was successfully posted. Thanks!')
+            return redirect(url_for('tasks'))
+        else:
+            flash('All fields are required')
+            return redirect(url_for('tasks'))
+    return render_template('tasks.html', form=form)
 
 
 # Mark tasks as complete
