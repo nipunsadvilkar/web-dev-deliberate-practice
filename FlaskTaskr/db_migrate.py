2927d6f740d797a7f39d943363712212c6006185
Nipun Sadvilkar <nipunsadvilkar@gmail.com>
2018-05-14 23:10:56 +0530

    Add admin & user role to users table

--- FlaskTaskr/db_migrate.py
+++ FlaskTaskr/db_migrate.py
@@ -4,7 +4,7 @@ from _config import DATABASE_PATH
 import sqlite3
 from datetime import datetime
 
-
+# migration of tasks table
 with sqlite3.connect(DATABASE_PATH) as conn:
 
     c = conn.cursor()
@@ -23,3 +23,22 @@ with sqlite3.connect(DATABASE_PATH) as conn:
                   posted_date, user_id) VALUES (?, ?, ?, ?, ?, ?)""", data)
 
     c.execute('DROP TABLE old_tasks')
+
+# migration of users table
+with sqlite3.connect(DATABASE_PATH) as conn:
+
+    c = conn.cursor()
+
+    c.execute('ALTER TABLE users RENAME TO old_users')
+
+    db.create_all()
+
+    c.execute("""SELECT name, email, password
+              FROM old_users ORDER BY id ASC""")
+
+    data = [(row[0], row[1], row[2], 'user') for row in c.fetchall()]
+
+    c.executemany("""INSERT INTO users (name, email, password, role) VALUES
+                  (?, ?, ?, ?)""", data)
+
+    c.execute('DROP TABLE old_users')
